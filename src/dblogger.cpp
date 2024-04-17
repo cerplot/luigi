4bc9381a58634ef6f3c78f961085dbfcf4a086cf
drahmedov <drahmedov@polymer-us.com>
2024-04-17 16:30:55 -0400

    Renaming

--- src/dblogger.cpp
+++ src/dblogger.cpp
@@ -7,6 +7,16 @@
 #include <unistd.h>
 #include <sys/syscall.h>
 
+/// Set affinity for current thread to be pinned to the provided core_id.
+inline auto setThreadCore(int core_id) noexcept {
+    cpu_set_t cpuset;
+
+    CPU_ZERO(&cpuset);
+    CPU_SET(core_id, &cpuset);
+
+    return (pthread_setaffinity_np(pthread_self(), sizeof(cpu_set_t), &cpuset) == 0);
+}
+
 template<typename T, typename... A>
 inline auto createAndStartThread(int core_id, const std::string &name, T &&func, A &&... args) noexcept {
     std::atomic<bool> running(false), failed(false);
@@ -73,6 +83,7 @@ private:
     std::atomic<size_t> next_read_index_ = {0};
     std::atomic<size_t> num_elements_ = {0};
 };
+
 
 class Logger {
 public:
